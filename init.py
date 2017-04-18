8e01d79991a9734ce7def51cae1d81b0c85d3f76
Thony Price <thonyprice@gmail.com>
2017-04-18 14:31:22 +0200

    persontable etc.

--- init.py
+++ init.py
@@ -1,36 +1,52 @@
-
 import psycopg2
-
 # http://initd.org/psycopg/docs/usage.html
 
-# Connect to an existing database
-conn = psycopg2.connect("dbname=hospital user=postgres")
 
-# Open a cursor to perform database operations
-cur = conn.cursor()
+# Drop table person
+def dropTable(name, cur):
+    string = 'DROP TABLE ' + name + ";"
+    cur.execute(string)  
+    return
 
 # Execute a command: this creates a new table
-cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
+def mkPerson(cur):
+    cur.execute("CREATE TABLE Person (  id serial PRIMARY KEY,  \
+                                        name varchar,           \
+                                        age integer,            \
+                                        gender char);")
+    i = 1
+    while i < 11:
+        cur.execute("INSERT INTO person (id, name, age, gender) VALUES (%s, %s, %s, %s)",
+        (i, "Person" + str(i), 30+i, gender(i))
+        )
+        i += 1
+    return
 
-# Pass data to fill a query placeholders and let Psycopg perform
-# the correct conversion (no more SQL injections!)
-names = ["Ann", "Bob", "Chris", "Diane", "Eric", "Fiona", "Gary", "Niklas"]
-nameId = 3000
-for name in names:
-    cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)",
-    (nameId, name)
-    )
-    nameId += 1
+def mkPatient(cur):
+    cur.execute("SELECT * FROM person;")
+    data = cur.fetchall()
+    print data
+    return
 
-# Query the database and obtain data as Python objects
-var = cur.execute("SELECT * FROM test;")
-cur.fetchone()
-(1, 100, "abc'def")
+def gender(num):
+    if (num%2 == 0):
+        return "M"
+    return "F" 
 
 
-# Make the changes to the database persistent
-conn.commit()
-
-# Close communication with the database
-cur.close()
-conn.close()
+def main():
+    # Connect to an existing database
+    conn = psycopg2.connect("dbname=hospital user=postgres")
+    # Open a cursor to perform database operations
+    cur = conn.cursor()
+    dropTable('person', cur)
+    mkPerson(cur)
+    mkPatient(cur)
+    # Make the changes to the database persistent
+    conn.commit()
+    # Close communication with the database
+    cur.close()
+    conn.close()
+    
+if __name__ == '__main__':
+    main()
