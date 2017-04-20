126ffd46226b5ebb23e44bfacd06e7d950d27539
Thony Price <thonyprice@gmail.com>
2017-04-20 16:45:49 +0200

    New issues and treatments

--- init.py
+++ init.py
@@ -50,15 +50,27 @@ def gender(num):
     return "F" 
 
 def mkTreatments(cur):
-    cur.execute("CREATE TABLE Treatments(   issue varchar PRIMARY KEY,  \
+    cur.execute("CREATE TABLE Treatments(   issue varchar,  \
                                             treatment varchar);")    
-    for i in range(1,11):
+    issues = ['Headace', 'Concussion', 'Cancer', 'Dehydration', 'Broken bone',
+            'Allegric', 'Obesity', 'Anorectic', 'Paranoia', 'Snake bite']
+    treats = ['Rest', 'Surgery', 'Drip', 'Acupunctur', 'Spa tretment',
+            'Lobotomize', 'Marijuana', 'Exercise', 'Gastric bypass', 'Insulin treatment',
+            'Autopsy', 'Biopsy', 'Blood test', 'CAT-scan', 'X-ray',
+            'Teeth brushing', 'Colesterole test', 'Condoms', 'Dialysis', 'EKG',
+            'ER-treatment', 'Eye operation', 'Facelift', 'Vaccination', 'Hair removal',
+            'Hard surgery', 'Iron lung', 'MRI-scan', 'Nerve operation', 'Nose job']
+    for i in range(len(issues)):
         cur.execute("INSERT INTO Treatments (issue, treatment) VALUES (%s, %s)",
-        (str(i), str(i)))
+        (str(issues[i]), str(treats[i])))
+        cur.execute("INSERT INTO Treatments (issue, treatment) VALUES (%s, %s)",
+        (str(issues[i]), str(treats[i+10])))
+        cur.execute("INSERT INTO Treatments (issue, treatment) VALUES (%s, %s)",
+        (str(issues[i]), str(treats[i+20])))
     return
 
 def gtIssues(cur):
-    cur.execute("SELECT issue FROM Treatments;")
+    cur.execute("SELECT DISTINCT issue FROM Treatments;")
     res_list = []
     for row in cur.fetchall():
         res_list += [row[0]]
@@ -76,12 +88,13 @@ def main():
     dropTable('treatments', cur)
     mkTreatments(cur)
     issue_list = gtIssues(cur)
+    # Make the changes to the database persistent
+    conn.commit()
     # mkPatient(cur) This might not be necessary
     # Create tkinter Nurse's form window
     app1 = app.Application(issue_list)
     app1.mainloop()
-    # Make the changes to the database persistent
-    conn.commit()
+
     # Close communication with the database
     cur.close()
     conn.close()
