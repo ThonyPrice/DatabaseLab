759cc42d4e0a057aa83aad32a9fd92bc59c4a642
Niklas Lindqvist <niklasdator@n144-p120.kthopen.kth.se>
2017-04-21 16:11:56 +0200

    Updated issues, added getTeam and getQueue (not working)

--- init.py
+++ init.py
@@ -2,12 +2,13 @@ import random
 import psycopg2
 # http://initd.org/psycopg/docs/usage.html
 import tkinter_test as app
+import tkinter_nurs2 as app2
 
 
 # Drop table person
 def dropTable(name, cur):
     string = 'DROP TABLE ' + name + ";"
-    cur.execute(string)  
+    cur.execute(string)
     return
 
 # Execute a command: this creates a new table
@@ -21,7 +22,7 @@ def mkPerson(cur):
     for i in range(1, 11):
         cur.execute("INSERT INTO person (id, name, age, gender, issue, priority) \
                     VALUES (%s, %s, %s, %s, %s, %s)",
-                    (i, "Person" + str(i), 30+i, gender(i), i, 
+                    (i, "Person" + str(i), 30+i, gender(i), i,
                     random.randrange(1,6))
                     )
     return
@@ -30,7 +31,7 @@ def mkPatient(cur):
     cur.execute("SELECT * FROM person;")
     data = cur.fetchall()
     return
-    
+
 def mkTeam(cur):
     cur.execute("CREATE TABLE Team (    teamId serial PRIMARY KEY,  \
                                         head varchar,               \
@@ -40,20 +41,19 @@ def mkTeam(cur):
 
     for i in range(1, 6):
         cur.execute("INSERT INTO Team (teamId, head, skill1, skill2, skill3) VALUES (%s, %s, %s, %s, %s)",
-        (i, "Head" + str(i), str(random.randrange(1, 11)), 
-        str(random.randrange(1, 11)), str(random.randrange(1, 11))))
+        (i, "Head" + str(i), str(issues[random.randrange(10)]),
+        str(issues[random.randrange(10)]), str(issues[random.randrange(10)])))
     return
 
 def gender(num):
     if (num%2 == 0):
         return "M"
-    return "F" 
+    return "F"
 
 def mkTreatments(cur):
     cur.execute("CREATE TABLE Treatments(   issue varchar,  \
-                                            treatment varchar);")    
-    issues = ['Headace', 'Concussion', 'Cancer', 'Dehydration', 'Broken bone',
-            'Allegric', 'Obesity', 'Anorectic', 'Paranoia', 'Snake bite']
+                                            treatment varchar);")
+
     treats = ['Rest', 'Surgery', 'Drip', 'Acupunctur', 'Spa tretment',
             'Lobotomize', 'Marijuana', 'Exercise', 'Gastric bypass', 'Insulin treatment',
             'Autopsy', 'Biopsy', 'Blood test', 'CAT-scan', 'X-ray',
@@ -83,6 +83,20 @@ def mkQueue(cur):
                                         priority int)")
     return
 
+
+def getTeam(cur, data):
+    skill = data[4]
+    team = cur.execute("SELECT teamid from Team WHERE skill1 == "+ "'" +skill+ "+"'"+ OR skill2 == "+skill+" OR skill3 == "+skill)
+    return team
+
+def getQue(cur, team):
+    que = cur.execute("SELECT * from Que WHERE teamid == "+team)
+    return que
+
+
+issues = ['Headace', 'Concussion', 'Cancer', 'Dehydration', 'Broken bone',
+        'Allegric', 'Obesity', 'Anorectic', 'Paranoia', 'Snake bite']
+
 def main():
     # Connect to an existing database
     conn = psycopg2.connect("dbname=hospital user=postgres")
@@ -103,10 +117,22 @@ def main():
     # Create tkinter Nurse's form window
     app1 = app.Application(issue_list)
     app1.mainloop()
-    print app1.data
+    print(app1.data)
+    team = getTeam(cur, app1.data)
+    print(team)
+    que = getQue(cur, team)
+    print(que)
+
+
+
+
+    #app2 = app.Application(queue)
+    #app2.mainloop()
+
+
     # Close communication with the database
     cur.close()
     conn.close()
-    
+
 if __name__ == '__main__':
     main()
