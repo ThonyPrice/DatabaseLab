ad88349f5238c9d2a0f405ffea66fc5686330ce8
Thony Price <thonyprice@gmail.com>
2017-04-22 12:30:26 +0200

    Added timestamps to queue_data

--- db_data.py
+++ db_data.py
@@ -3,27 +3,27 @@
 # Data for patients in Queue;
 #   TeamID, Name, Age, Gender, Issue, Priority
 queue_data = [
-    ['1', 'Adam',   23, 'M', 'Broken neck',     3],
-    ['1', 'Berit',  45, 'F', 'Broken arm',      4],
-    ['1', 'Carl',   54, 'M', 'Broken back',     2],
-    ['1', 'Dan',    4,  'M', 'Broken arm',      1],
-    ['2', 'Erik',   77, 'M', 'Snake bite',      4],
-    ['2', 'Fiona',  24, 'F', 'Dog bite',        2],
-    ['2', 'Gaston', 64, 'M', 'Cat bite',        3],
-    ['2', 'Hans',   59, 'M', 'Cat bite',        5],
-    ['2', 'Iris',   71, 'F', 'Dog bite',        4],
-    ['3', 'Juan',   8,  'M', 'Chest pain',      1],
-    ['3', 'Kitty',  33, 'F', 'Back pain',       2],
-    ['3', 'Lena',   74, 'F', 'Foot pain',       2],
-    ['3', 'Max',    28, 'M', 'Back pain',       5],
-    ['4', 'Nora',   15, 'F', 'Food allergy',    3],
-    ['4', 'Oden',   55, 'M', 'Flower allergy',  5],
-    ['4', 'Pat',    82, 'M', 'Sun allergy',     4],
-    ['4', 'Rick',   28, 'M', 'Sun allergy',     3],
-    ['5', 'Siri',   51, 'F', 'Impact trauma',   2],
-    ['5', 'Tage',   40, 'M', 'Serious trauma',  4],
-    ['5', 'Ulf',    80, 'M', 'Fake trauma',     3],
-    ['5', 'Vera',   66, 'F', 'Impact trauma',   2]
+    ['1', 'Adam',   23, 'M', 'Broken neck',     3,  '13:00'],
+    ['1', 'Berit',  45, 'F', 'Broken arm',      4,  '14:00'],
+    ['1', 'Carl',   54, 'M', 'Broken back',     2,  '13:45'],
+    ['1', 'Dan',    4,  'M', 'Broken arm',      1,  '15:00'],
+    ['2', 'Erik',   77, 'M', 'Snake bite',      4   '14:45'],
+    ['2', 'Fiona',  24, 'F', 'Dog bite',        2,  '15:15'],
+    ['2', 'Gaston', 64, 'M', 'Cat bite',        3,  '14:30'],
+    ['2', 'Hans',   59, 'M', 'Cat bite',        5,  '13:00'],
+    ['2', 'Iris',   71, 'F', 'Dog bite',        4,  '13:56'],
+    ['3', 'Juan',   8,  'M', 'Chest pain',      1,  '15:22'],
+    ['3', 'Kitty',  33, 'F', 'Back pain',       2,  '13:11'],
+    ['3', 'Lena',   74, 'F', 'Foot pain',       2,  '14:47'],
+    ['3', 'Max',    28, 'M', 'Back pain',       5,  '13:30'],
+    ['4', 'Nora',   15, 'F', 'Food allergy',    3,  '13:37'],
+    ['4', 'Oden',   55, 'M', 'Flower allergy',  5,  '14:12'],
+    ['4', 'Pat',    82, 'M', 'Sun allergy',     4,  '11:06'],
+    ['4', 'Rick',   28, 'M', 'Sun allergy',     3,  '12:12'],
+    ['5', 'Siri',   51, 'F', 'Impact trauma',   2,  '14:59'],
+    ['5', 'Tage',   40, 'M', 'Serious trauma',  4,  '13:13'],
+    ['5', 'Ulf',    80, 'M', 'Fake trauma',     3,  '12:48'],
+    ['5', 'Vera',   66, 'F', 'Impact trauma',   2,  '13:55']
 ]
 
 # Data for Teams;
